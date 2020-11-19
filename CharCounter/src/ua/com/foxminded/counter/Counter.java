c53ff8aceff09e91b9031144b4dde53afdb75087
Egor Anchutin <zbit16@gmail.com>
2020-11-19 18:19:46 +0300

    Add method to get unique set of unique chars in line

--- CharCounter/src/ua/com/foxminded/counter/Counter.java
+++ CharCounter/src/ua/com/foxminded/counter/Counter.java
@@ -1,4 +1,17 @@
 package ua.com.foxminded.counter;
 
+import java.lang.reflect.Array;
+import java.util.*;
+
 public class Counter {
+    public Set<Character> getUniqueCharsFromString(String line) {
+        char[] charsIntermediate = line.toCharArray();
+        Set<Character> charsSet = new LinkedHashSet<>();
+
+        for (char c : charsIntermediate) {
+            charsSet.add(c);
+        }
+
+        return charsSet;
+    }
 }
